69cc38636441203f1e0c89293adefb60f10755e2
naftalysh <naftalysh@gmail.com>
2016-11-02 14:34:31 +0200

    2/11/2016 new

--- ENCOG_DEMO2/NumbersAnalysis.cs
+++ ENCOG_DEMO2/NumbersAnalysis.cs
@@ -8,21 +8,21 @@ namespace LotoPrediction
 {
     public class NumbersAnalysis
     {
-        private Dictionary<int, int> _NumbersDic37_Total = new Dictionary<int, int>();
-        private Dictionary<int, int> _NumbersDic7_Total = new Dictionary<int, int>();
+        private Dictionary<Int32, Int32> _NumbersDic37_Total = new Dictionary<Int32, Int32>();
+        private Dictionary<Int32, Int32> _NumbersDic7_Total = new Dictionary<Int32, Int32>();
 
-        private Dictionary<int, int> _NumbersDic37_PastWindow = new Dictionary<int, int>();
-        private Dictionary<int, int> _NumbersDic7_PastWindow = new Dictionary<int, int>();
+        private Dictionary<Int32, Int32> _NumbersDic37_PastWindow = new Dictionary<Int32, Int32>();
+        private Dictionary<Int32, Int32> _NumbersDic7_PastWindow = new Dictionary<Int32, Int32>();
 
 
-        public IDictionary<int, int> NumbersDic37_Total { get; set; }
-        public IDictionary<int, int> NumbersDic7_Total { get; set; }
-        public IDictionary<int, int> NumbersDic37_PastWindow { get; set; }
-        public IDictionary<int, int> NumbersDic7_PastWindow { get; set; }
+        public IDictionary<Int32, Int32> NumbersDic37_Total { get; set; }
+        public IDictionary<Int32, Int32> NumbersDic7_Total { get; set; }
+        public IDictionary<Int32, Int32> NumbersDic37_PastWindow { get; set; }
+        public IDictionary<Int32, Int32> NumbersDic7_PastWindow { get; set; }
 
 
         public NumbersAnalysis() {
-            int i;
+            Int32 i;
 
             for (i = 1; i <= 37; i++) _NumbersDic37_Total[i] = 0;
             for (i = 1; i <= 7; i++) _NumbersDic7_Total[i] = 0;
